89edc6ed5e39e6d6fabb433afeea2bd2f0311381
mentle1993 <mentle1993ss@gmail.com>
2021-04-24 14:10:19 +0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+// 
+const name = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade');
+sayHi('susan')
+sayHi(name.john)
+sayHi(name.peter)
